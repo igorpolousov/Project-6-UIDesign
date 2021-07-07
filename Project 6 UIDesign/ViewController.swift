60201ed24512d1dfddb49cb611c495ec163773aa
Igor Polousov <igorpolousov@gmail.com>
2021-07-07 13:57:59 +0300

    Initial Commit

---
+++ Project 6 UIDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 6 UIDesign
+//
+//  Created by Igor Polousov on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
