28a5c52d9ddd9ea6508d4ccab3a2fedd4cee1435
Igor Polousov <igorpolousov@gmail.com>
2021-07-08 13:07:50 +0300

    + описание к сделанным работам

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  Project6b
-//
+//  Day 30
 //  Created by Igor Polousov on 07.07.2021.
 //
 
@@ -11,11 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Создали label
         let label1 = UILabel()
+        // Сделали чтобы небыло autoresizing for constraints
         label1.translatesAutoresizingMaskIntoConstraints = false
+        // Задали цвет фона
         label1.backgroundColor = UIColor.red
+        // Задали текст
         label1.text = "THESE"
+        // Задали расположение view inside super view
         label1.sizeToFit()
         
         let label2 = UILabel()
@@ -42,18 +46,22 @@ class ViewController: UIViewController {
         label5.text = "LABELS"
         label5.sizeToFit()
         
+        // Добавили view on superview
         view.addSubview(label1)
         view.addSubview(label2)
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
-        
+        // Создали словарь чтобы упростить работу в дальнейшем
         let viewsDictionary = ["label1": label1,"label2": label2,"label3": label3,"label4": label4,"label5": label5]
-        
+        // Ограничения по горизонтали для каждой view
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        // Словарь для работ с горизонтальными constraints
+        let metrics = ["lableHight": 88]
+        // Ограничения по вертикали для каждой view. Размер и приоритет @999 установлено для первой view остальные сделаны по образцу первой
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(lableHight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         
     }
 
